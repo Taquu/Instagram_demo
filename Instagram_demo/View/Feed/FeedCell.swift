bedb8f952691f3127c39cfbf846670d4ee229f26
Takuya Ashinmuira <takuya.ashimura@gmail.com>
2021-03-07 06:33:47 +0900

    FeedCell

--- Instagram_demo/View/Feed/FeedCell.swift
+++ Instagram_demo/View/Feed/FeedCell.swift
@@ -10,15 +10,67 @@ import SwiftUI
 struct FeedCell: View {
     var body: some View {
         VStack(alignment: .leading){
+            // user info
             HStack {
+                Image("yellow-bucks")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 36, height: 36)
+                    .clipped()
+                    .cornerRadius(18)
+                
+                Text("¥ellowBucks")
+                    .font(.system(size: 14, weight:.bold))
                 
             }
             
             // post image
+            Image("yellow-bucks")
+                .resizable()
+                .scaledToFill()
+                .frame(maxHeight: 440)
+                .clipped()
             
             // action button
+            HStack(spacing: 8) {
+                Button(action: {}, label: {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 28, height: 28)
+                        .font(.system(size: 20))
+                        .padding(4)
+                })
+                
+                Button(action: {}, label: {
+                    Image(systemName: "bubble.right")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 28, height: 28)
+                        .font(.system(size: 20))
+                        .padding(4)
+                })
+                
+                Button(action: {}, label: {
+                    Image(systemName: "paperplane")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 28, height: 28)
+                        .font(.system(size: 20))
+                        .padding(4)
+                })
+            }.foregroundColor(.black)
             
             // caption
+            HStack {
+                Text("¥ellowBucks").font(.system(size: 14, weight: .semibold)) + Text(" HipHop is my life. Are U guys have fun with my music? They never do what they say.")
+                        .font(.system(size: 15))
+            }
+            
+            Text("2d")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+                .padding(.top)
         }
     }
 }
